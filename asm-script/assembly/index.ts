f62bb3b435562c09f127c3cac1dd68c89c7b068f
Rodrigo Kumpera <kumpera@gmail.com>
2021-10-15 13:41:24 -0400

    update

--- asm-script/assembly/index.ts
+++ asm-script/assembly/index.ts
@@ -1,9 +1,17 @@
 export declare namespace vw {
   export function read_feature(ns: string, f: string): f32
   export function write_feature(ns: string, f: string, val: f32): void
+  export function remove_feature(ns: string, f: string): void
 }
 
+function rename_feature(ns: string, old_name: string, new_name: string) : void
+{
+  var value = vw.read_feature(ns, old_name);
+  vw.remove_feature(ns, old_name);
+  vw.write_feature(ns, new_name, value);
+}
+
+//This is the entrypoint to a processing script
 export function process_example() : void {
-  var f = vw.read_feature("f", "t1");
-  vw.write_feature("f", "t1000", Math.log(f + 1) as f32);
+  rename_feature("f", "t1", "t1000");
 }
